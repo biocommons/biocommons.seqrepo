b5a18fae113f7f0fcaaa088a0a223d1553fca504
Reece Hart <reecehart@gmail.com>
2017-07-15 13:29:57 -0700

    added misc/threading-verification.py

---
+++ misc/threading-verification.py
@@ -0,0 +1,73 @@
+# See https://github.com/biocommons/biocommons.seqrepo/issues/11
+# Issue: seqrepo raises exceptions in (some) multithreaded environments
+
+# Here's my understanding: sqlite support for multthreading (Python or
+# otherwise) varies by version, platform, and compilation flags.  It
+# is always safe to create separate readers after spawning threads.
+# Alternatively, depending on sqlite library support, it *may* be
+# possible to share an instances across threads by allocating before
+# spawning threads.  Furthermore, some versions of sqlite issue
+# warnings if the library is used in a threaded environment without
+# check_same_thread=False, but that this check is advisory only (i.e.,
+# it doesn't change library behavior).
+
+# For Reece:
+# sys.platform: linux2
+# sys.version: 2.7.13 (default, Jan 19 2017, 14:48:08)  [GCC 6.3.0 20170118]
+# sqlite3.sqlite_version: 3.16.2
+# ('SeqRepo(root_dir=/tmp/sr, writeable=False)', (8281, 8280, 'SMELLASSWEET'))
+# ('SeqRepo(root_dir=/tmp/sr, writeable=True)', (8283, 8280, 'SMELLASSWEET'))
+
+
+
+import os
+from multiprocessing import Process, Queue
+import sqlite3
+import sys
+
+from biocommons.seqrepo import SeqRepo
+
+
+def fetch_in_thread(sr, nsa):
+    """fetch a sequence in a thread
+
+    """
+
+    def fetch_seq(q, nsa):
+        pid, ppid = os.getpid(), os.getppid()
+        q.put((pid, ppid, sr[nsa]))
+    
+    q = Queue()
+    p = Process(target=fetch_seq, args=(q, nsa))
+    p.start()
+    pid, ppid, seq = q.get()
+    p.join()
+
+    assert pid != ppid, "sequence was not fetched from thread"
+    return pid, ppid, seq
+    
+
+def make_seqrepo(writeable):    
+    sr = SeqRepo("/tmp/sr", writeable=True)
+    sr.store("SMELLASSWEET", [{"namespace": "en", "alias": "rose"}, {"namespace": "fr", "alias": "rose"}])
+
+    if writeable == False:
+        del sr
+        sr = SeqRepo("/tmp/sr", writeable=writeable)
+
+    return sr
+
+
+if __name__ == "__main__":
+    nsa = "en:rose"
+
+    def _test(sr):
+        r = fetch_in_thread(sr, nsa)
+        print(str(sr), r)
+
+    print("sys.platform: " + sys.platform)
+    print("sys.version: " + sys.version.replace("\n", " "))
+    print("sqlite3.sqlite_version: " + sqlite3.sqlite_version)
+    
+    _test(make_seqrepo(writeable=False))
+    _test(make_seqrepo(writeable=True))
