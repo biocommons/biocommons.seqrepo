fb6d88682cb73ee6971cfa47d4dcd90a9c649167
Alex Henrie <alexhenrie24@gmail.com>
2019-04-09 18:09:37 -0600

    Use builtin functions instead of polyfill functions whenever possible (#56)

--- biocommons/seqrepo/py2compat/__init__.py
+++ biocommons/seqrepo/py2compat/__init__.py
@@ -3,12 +3,24 @@ import io
 
 import six
 
+try:
+    from functools import lru_cache    # Python >= 3.2
+except ImportError:
+    from ._lru_cache import lru_cache
+
+try:
+    from shutil import which    # Python >= 3.3
+except ImportError:
+    from ._which import which
+
+try:
+    from os.path import commonpath    # Python >= 3.5
+except ImportError:
+    from ._commonpath import commonpath
+
 if six.PY2:    # pragma: no cover
 
-    from ._lru_cache import lru_cache
     from ._makedirs import makedirs, FileExistsError
-    from ._commonpath import commonpath
-    from ._which import which
 
     def gzip_open_encoded(file, encoding=None):
         return io.TextIOWrapper(io.BufferedReader(gzip.open(file)), encoding="utf8")
@@ -16,10 +28,6 @@ if six.PY2:    # pragma: no cover
 else:    # pragma: no cover
 
     from os import makedirs     # flake8: noqa
-    from os.path import commonpath
-    from functools import lru_cache
-    from shutil import which   # >= Python 3.3
-
     FileExistsError = FileExistsError
 
     def gzip_open_encoded(file, encoding=None):
