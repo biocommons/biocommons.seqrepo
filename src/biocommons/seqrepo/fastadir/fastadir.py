fa7d4133c911578f94234363464d6797feafdae6
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-06-13 10:06:21 -0400

    refactor: don't use deprecated timezone-ignorant datetime methods

--- src/biocommons/seqrepo/fastadir/fastadir.py
+++ src/biocommons/seqrepo/fastadir/fastadir.py
@@ -194,7 +194,7 @@ class FastaDir(BaseReader, BaseWriter):
         #       <------ dir_ ----->
         #       <----------- path ----------->
         if self._writing is None:
-            reldir = datetime.datetime.utcnow().strftime("%Y/%m%d/%H%M")
+            reldir = datetime.datetime.now(datetime.UTC).strftime("%Y/%m%d/%H%M")
             basename = str(time.time()) + ".fa.bgz"
             relpath = os.path.join(reldir, basename)
 
